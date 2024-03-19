0c1f69f7a6ed4618fa054e60249c31a9b5d62dfe
Keren_Apuque <91158924+KeReN995221@users.noreply.github.com>
2024-03-19 15:45:28 -0300

    Update os.cpp

--- so-sim-cpp-exercicio/os.cpp
+++ so-sim-cpp-exercicio/os.cpp
@@ -12,11 +12,13 @@
 
 
 namespace OS {
-
+	Arch::Terminal *terminal;
+	std::vector<int> mensagem;
 // ---------------------------------------
 
 void boot (Arch::Terminal *terminal, Arch::Cpu *cpu)
 {
+	OS::terminal = terminal;
 	terminal->println(Arch::Terminal::Type::Command, "Type commands here");
 	terminal->println(Arch::Terminal::Type::App, "Apps output here");
 	terminal->println(Arch::Terminal::Type::Kernel, "Kernel output here");
@@ -26,37 +28,30 @@ void boot (Arch::Terminal *terminal, Arch::Cpu *cpu)
 
 void interrupt (const Arch::InterruptCode interrupt)
 {
-	switch (interrupt)
-	{
-		case (Keyboard):
-			int key_pressed = read_typed_char();
-			
-			if(is_backspace(key_pressed) == true){
-				std::cout << _SPACE;
-			}
-			else if(is_alpha(key_pressed) == true){
-				std::cout << static_cast<char>(key_pressed);
+	if(interrupt == Arch::InterruptCode::Keyboard){
+		int teclado = terminal->read_typed_char();
 
+		if(terminal->is_backspace(teclado)){
+			mensagem.pop_back();
+			terminal->print(Arch::Terminal::Type::Kernel, "\r");
+			for(int i = 0; i < mensagem.size(); i++){
+				terminal->print(Arch::Terminal::Type::Kernel, static_cast <char> (mensagem[i]));
 			}
-			else if(is_num(key_pressed) == true){
-				std::cout << static_cast<char>(key_pressed);
-			}
-			else if (is_return(key_pressed) == true){
-				std::cout << std::endl; 
-
-			}	
-			break;
-		case (Timer):
-
-		case(GPF):
-		default:
-			break;
+		}else if(terminal->is_alpha(teclado)){
+			mensagem.push_back(teclado);
+			terminal->print(Arch::Terminal::Type::Kernel, static_cast <char> (teclado));
+		}
+		else if(terminal->is_num(teclado)){
+			mensagem.push_back(teclado);
+			terminal->print(Arch::Terminal::Type::Kernel, static_cast <char> (teclado));
+		}
+		else if(terminal->is_return(teclado)){
+			mensagem.clear();
+			terminal->print(Arch::Terminal::Type::Kernel, "\n");
+		}
 	}
 }
-	//parar de rodar o terminal
-	// mostrar ao usuário 
-	
-	// algum momento vou ter de mostrar o que o usuário está digita}
+
 
 // ---------------------------------------
 
